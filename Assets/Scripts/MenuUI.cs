19d85c9f5b504a6f58d69550a2ff240152a12e4c
rschnei9 <rschnei9@student.fitchburgstate.edu>
2026-02-16 19:47:52 -0500

    Can't get the UI to work
    I tried

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+public class MenuUI : MonoBehaviour
+{
+    public TextMeshProUGUI myUI;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    ToString()
+    {
+        myUI.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
